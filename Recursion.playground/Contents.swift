75140cea78eadbe13d13b6917ec16748facc6438
Steve Lipton <steve@makeapppie.com>
2018-08-23 06:47:21 -0500

    Added correct signature comments

--- Recursion.playground/Contents.swift
+++ Recursion.playground/Contents.swift
@@ -1,3 +1,11 @@
+//
+//  A exercise file for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
+//
+
+
 //: Recursion Demo
 //: Definintion of recursion: See Recursion
 
--- RecursionDemo/ViewController.swift
+++ RecursionDemo/ViewController.swift
@@ -2,8 +2,11 @@
 //  ViewController.swift
 //  RecursionDemo
 //
-//  Created by Steven Lipton on 8/22/18.
-//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+//  A exercise file for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
 //
 
 import UIKit
